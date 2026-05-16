eefc3082215105420bac988e844485b9e3c28a81
Grant <grantt7070@gmail.com>
2026-05-16 00:11:25 -0400

    Edited server.js

--- server/server.js
+++ server/server.js
@@ -99,6 +99,8 @@ app.delete('/api/favorites/:id', async (req, res) => {
   }
 )
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+if (require.main === module) {
+  app.listen(port, () => console.log(`Server running on port ${port}`))
+}
+
+module.exports = app;
